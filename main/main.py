e6409b24894d5c69ed92f843c53c56b73f62177a
Felipe Held <lizardshit123@gmail.com>
2018-10-19 00:21:26 -0300

    addition of rectangle sides calculations

--- main/main.py
+++ main/main.py
@@ -19,8 +19,10 @@ ball = Ball(screen, color = (150, 200, 0), position = [400, 300], radius = 10)
 one_rectangle = Rectangle(screen, color = (231, 43, 120), area = (40, 40, 40, 30))
 
 pos_x_rect, pos_y_rect, rect_width, rect_height = one_rectangle.area
-left_side = set([(pos_x_rect, y) for y in range(pos_y_rect, pos_y_rect + rect_height + 1)])
-right_side = set([(pos_x_rect + rect_width, y) for y in range(pos_y_rect, pos_y_rect + rect_height + 1)])
+left_side = [[pos_x_rect, y] for y in range(pos_y_rect, pos_y_rect + rect_height + 1)]
+right_side = [[pos_x_rect + rect_width, y] for y in range(pos_y_rect, pos_y_rect + rect_height + 1)]
+top_side = [[x, pos_y_rect] for x in range(pos_x_rect, pos_x_rect + rect_width + 1)]
+bottom_side = [[x, pos_y_rect + rect_height] for x in range(pos_x_rect + rect_width + 1)]
 # game object variables #
 print(left_side)
 print(right_side)
@@ -45,6 +47,11 @@ while True:
     pos_x_circle, pos_y_circle = pos_x_circle + mov_x, pos_y_circle + mov_y
     ball.position = pos_x_circle, pos_y_circle
 
+    if [ball.position[0] - ball.radius, ball.position[1]] in right_side:
+        mov_x = 1
+    if [ball.position[0], ball.position[1] - ball.radius] in bottom_side:
+        mov_y = 1
+
     if pos_x_circle + ball.radius >= screen_width:
         mov_x = -1
     elif pos_x_circle - ball.radius <= 0:
@@ -62,6 +69,6 @@ while True:
         if event.type == pygame.QUIT:
             pygame.quit()
 
-    clock.tick(300)
+    clock.tick(450)
     pygame.display.flip()
 # main game loop #
